f302bdcaf2657840574a7edb9b98590480f28c66
rajshekar11 <rajshekar767@gmail.com>
2022-11-15 18:43:12 +0530

    scrolling removed

--- main.js
+++ main.js
@@ -22,62 +22,62 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50;
+//         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 500,
-    delay: 80,
-     reset: true
-});
+// /*===== SCROLL REVEAL ANIMATION =====*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 500,
+//     delay: 80,
+//      reset: true
+// });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 100}); 
-sr.reveal('.home__social-icon',{ interval: 80}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 80}); 
+// sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+// sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 100}); 
+// sr.reveal('.home__social-icon',{ interval: 80}); 
+// sr.reveal('.skills__data, .work__img, .contact__input',{interval: 80}); 
 
-/*SCROLL HOME*/
-sr.reveal( ".home-title", {} );
-sr.reveal( ".button", { delay: 60 } );
-sr.reveal( ".home-img", { delay: 100 } );
-sr.reveal( ".home-social-icon", { interval: 100 } );
+// /*SCROLL HOME*/
+// sr.reveal( ".home-title", {} );
+// sr.reveal( ".button", { delay: 60 } );
+// sr.reveal( ".home-img", { delay: 100 } );
+// sr.reveal( ".home-social-icon", { interval: 100 } );
 
-/*SCROLL ABOUT*/
-sr.reveal( ".about-img", {} );
-sr.reveal( ".about-subtitle", { delay: 100 } );
-sr.reveal( ".about-text", { delay: 100 } );
+// /*SCROLL ABOUT*/
+// sr.reveal( ".about-img", {} );
+// sr.reveal( ".about-subtitle", { delay: 100 } );
+// sr.reveal( ".about-text", { delay: 100 } );
 
-// Scross main skills mainskills
-sr.reveal( ".mainskills", { interval: 100 } );
-sr.reveal( ".mskill-img", { delay: 100 } );
-sr.reveal( ".mskill-text", { delay: 100 } );
-/*SCROLL SKILLS*/
-sr.reveal( ".skills-subtitle", {} );
-sr.reveal( ".skills-text", {} );
-sr.reveal( ".skills-data", { interval: 100 } );
-// sr.reveal(".skills-img", { delay: 600 });
+// // Scross main skills mainskills
+// sr.reveal( ".mainskills", { interval: 100 } );
+// sr.reveal( ".mskill-img", { delay: 100 } );
+// sr.reveal( ".mskill-text", { delay: 100 } );
+// /*SCROLL SKILLS*/
+// sr.reveal( ".skills-subtitle", {} );
+// sr.reveal( ".skills-text", {} );
+// sr.reveal( ".skills-data", { interval: 100 } );
+// // sr.reveal(".skills-img", { delay: 600 });
 
-/*SCROLL projects*/
-sr.reveal( ".project-img", { interval: 80 } );
+// /*SCROLL projects*/
+// sr.reveal( ".project-img", { interval: 80 } );
 
-// SCROLL CONTACT
-sr.reveal(".contact-input", { interval: 100 });
+// // SCROLL CONTACT
+// sr.reveal(".contact-input", { interval: 100 });
